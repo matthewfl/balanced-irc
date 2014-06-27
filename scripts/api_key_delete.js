6ac46b46fcf98cb2e7651806ee739cd9a695812a
Matthew Francis-Landau <matthew@matthewfl.com>
2014-06-27 14:33:09 -0700

    give them a new api key after they posted theirs

--- scripts/api_key_delete.js
+++ scripts/api_key_delete.js
@@ -3,21 +3,26 @@ var match = /(ak-(test|prod)-[a-zA-Z0-9]+)/;
 var balanced = require('balanced-official')
 
 module.exports = function (act) {
-		act.on_message(function (msg, reply) {
+		act.on_message(function (msg, reply, info) {
 				var key = match.exec(msg);
 				if(key) {
 						console.log(key);
 						// omfg they did it again
 						balanced.configure(key[1]);
-						balanced.api_key.query.then(function (k) {
-								k.all().then(function(keys) {
-										for(var a=0; a<keys.length; a++) {
-												if(keys[a].secret) { // secret only comes back for the current api key
-														keys[a].unstore();
-														reply(true, 'your api key '+keys[a].secret+' has been deleted for your protection, please do not shared your marketplace secret key in irc as this channel is publically logged');
+						balanced.api_key.create().then(function (new_key) {
+								act.msg(info.nick, 'Here is your new api key, please do not publically share it again: '+new_key.secret);
+								balanced.api_key.query.then(function (k) {
+										k.all().then(function(keys) {
+												for(var a=0; a<keys.length; a++) {
+														if(keys[a].secret) { // secret only comes back for the current api key
+																keys[a].unstore();
+																reply(true, 'your api key '+keys[a].secret+' has been deleted for your protection, a new api has been sent to you in a private message, please do not shared your marketplace secret key in irc as this channel is publically logged');
+														}
 												}
-										}
+										});
 								});
+						}, function () {
+								console.log('api key was already invalid');
 						});
 				}
 		});
