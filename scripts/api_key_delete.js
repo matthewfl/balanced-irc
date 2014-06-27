88da9788aaaa48c8a0c3b4af837a51685294c8a4
Matthew Francis-Landau <matthew@matthewfl.com>
2014-06-27 14:47:39 -0700

    can't spell

--- scripts/api_key_delete.js
+++ scripts/api_key_delete.js
@@ -10,13 +10,13 @@ module.exports = function (act) {
 						// omfg they did it again
 						balanced.configure(key[1]);
 						balanced.api_key.create().then(function (new_key) {
-								act.msg(info.nick, 'Here is your new api key, please do not publically share it again: '+new_key.secret);
+								act.msg(info.nick, 'Here is your new api key, please do not publicly share it again: '+new_key.secret);
 								balanced.api_key.query.then(function (k) {
 										k.all().then(function(keys) {
 												for(var a=0; a<keys.length; a++) {
 														if(keys[a].secret) { // secret only comes back for the current api key
 																keys[a].unstore();
-																reply(true, 'your api key '+keys[a].secret+' has been deleted for your protection, a new api has been sent to you in a private message, please do not shared your marketplace secret key in irc as this channel is publically logged');
+																reply(true, 'your api key '+keys[a].secret+' has been deleted for your protection, a new api has been sent to you in a private message, please do not shared your marketplace secret key in irc as this channel is publicly logged');
 														}
 												}
 										});
